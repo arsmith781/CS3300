5f997ec33260c267769fb05facdaef8be1145b2d
arsmith781 <92188553+arsmith781@users.noreply.github.com>
2024-02-25 14:07:30 -0700

    student, portfolio, and project model done. Exmaple made in databse via admin

--- portfolio/portfolio_app/admin.py
+++ portfolio/portfolio_app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Student
+from .models import Student, Project, Portfolio
 
 # Register your models here.
 admin.site.register(Student)
+admin.site.register(Project)
+admin.site.register(Portfolio)
--- portfolio/portfolio_app/models.py
+++ portfolio/portfolio_app/models.py
@@ -3,6 +3,33 @@ from django.urls import reverse
 
 # Create your models here.
 
+
+class Portfolio(models.Model):
+    title = models.CharField(max_length=200)
+    contact_email = models.CharField(max_length=200)
+    is_active = models.BooleanField(default=False)
+    about = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('portfolio-detail', args=[str(self.id)])
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('project-detail', args=[str(self.id)])
+
+
 class Student(models.Model):
 
     # List of choices for major value in database, human readable name
@@ -18,6 +45,7 @@ class Student(models.Model):
     name = models.CharField(max_length=200)
     email = models.CharField("UCCS Email", max_length=200)
     major = models.CharField(max_length=200, choices=MAJOR)
+    portfolio = models.OneToOneField(Portfolio, null=True, on_delete=models.CASCADE, unique=True)
 
     def __str__(self):
         return self.name
@@ -27,5 +55,3 @@ class Student(models.Model):
     # add a "View on Site" button to the model's record editing screens in the Admin site
     def get_absolute_url(self):
         return reverse('student-detail', args=[str(self.id)])
-
-
