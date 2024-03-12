fcbb35f47a1b09e6d59db615b0959956e5ceaaa3
arsmith781 <92188553+arsmith781@users.noreply.github.com>
2024-03-12 11:53:59 -0600

    Done with home page, portfolio details, project list, project detail

--- portfolio/portfolio_app/urls.py
+++ portfolio/portfolio_app/urls.py
@@ -8,4 +8,11 @@ from . import views
 # example in html <a href="{% url 'index' %}">Home</a>.
 urlpatterns = [
     path('', views.index, name='index'),
+    path('students/', views.StudentListView.as_view(), name='students'),
+    path('student/<int:pk>', views.StudentDetailView.as_view(), name='student-detail'),
+    path('portfolios/', views.PortfolioListView.as_view(), name='portfolios'),
+    path('portfolio/<int:pk>', views.PortfolioDetailView.as_view(), name='portfolio-detail'),
+    path('projects/', views.ProjectListView.as_view(), name='projects'),
+    path('project/<int:pk>', views.ProjectDetailView.as_view(), name='project-detail'),
+
 ]
--- portfolio/portfolio_app/views.py
+++ portfolio/portfolio_app/views.py
@@ -1,7 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
+from django.views import generic
+
 
 def index(request):
-    # return HttpResponse('home page')
-    return render(request, 'portfolio_app/index.html')
+    studentActivePortfolios = Student.objects.select_related('portfolio').all().filter(portfolio__is_active=True)
+    return render(request, 'portfolio_app/index.html', {'studentActivePortfolios': studentActivePortfolios})
+
+
+class StudentListView(generic.ListView):
+    model = Student
+
+
+class StudentDetailView(generic.DetailView):
+    model = Student
+
+
+class PortfolioListView(generic.ListView):
+    model = Portfolio
+
+
+class PortfolioDetailView(generic.DetailView):
+    model = Portfolio
+
+
+class ProjectListView(generic.ListView):
+    model = Project
+
+
+class ProjectDetailView(generic.DetailView):
+    model = Project
 
